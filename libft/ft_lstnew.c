2691efd9f8a11256ceb7a452ba935a5e003e9c09
Moaaz Aboualyan <maboualy@u91z01s01.42kl.edu.my>
2025-05-27 19:02:21 +0800

    Some formatting for norminette and then changed ft_split to meet requirements (one char and not char * for comparison input)

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: maboualy <moaazahmedaboualyan@gmail.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 15:16:52 by maboualy          #+#    #+#             */
-/*   Updated: 2025/05/26 15:30:20 by maboualy         ###   ########.fr       */
+/*   Updated: 2025/05/27 17:37:55 by maboualy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,9 @@ t_list	*ft_lstnew(void *content)
 	t_list	*node;
 
 	node = malloc(sizeof(t_list));
-    if (!node)
-        return (0);
+	if (!node)
+		return (0);
 	node->content = content;
 	node->next = 0;
-
 	return (node);
 }
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,21 +6,21 @@
 /*   By: maboualy <moaazahmedaboualyan@gmail.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 15:16:18 by maboualy          #+#    #+#             */
-/*   Updated: 2025/05/26 13:18:16 by maboualy         ###   ########.fr       */
+/*   Updated: 2025/05/27 18:57:15 by maboualy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
+#include <stdio.h>
 
-static int	char_in_set(char c, char *charset);
-static int	count_words(char *str, char *charset);
-static int	word_index(char *str, char *charset, int index);
-static int	word_length(char *str, char *charset, int index);
+static int	count_words(char const *str, char c);
+static int	word_index(char const *str, char c, int index);
+static int	word_length(char const *str, char c, int index);
 
-char	**ft_split(char *s, char *c)
+char	**ft_split(char const *s, char c)
 {
 	int		i;
-	int		j;
 	char	**strs;
 
 	strs = malloc((count_words(s, c) + 1) * sizeof(char *));
@@ -29,37 +29,25 @@ char	**ft_split(char *s, char *c)
 	i = 0;
 	while (i < count_words(s, c))
 	{
-		j = 0;
-		strs[i] = malloc((word_length(s, c, i) + 1) * sizeof(char));
+		strs[i] = ft_substr(s, word_index(s, c, i), word_length(s, c, i));
 		if (strs[i] == 0)
-			return (0);
-		while (j < word_length(s, c, i))
 		{
-			strs[i][j] = s[(word_index(s, c, i) + j)];
-			j++;
+			i--;
+			while (i >= 0)
+			{
+				free(strs[i]);
+				i--;
+			}
+			free(strs);
+			return (0);
 		}
-		strs[i][j] = 0;
 		i++;
 	}
 	strs[i] = 0;
 	return (strs);
 }
 
-static int	char_in_set(char c, char *charset)
-{
-	int	i;
-
-	i = 0;
-	while (charset[i] != 0)
-	{
-		if (charset[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-static int	word_length(char *str, char *charset, int index)
+static int	word_length(char const *str, char c, int index)
 {
 	int	i;
 	int	count;
@@ -69,9 +57,9 @@ static int	word_length(char *str, char *charset, int index)
 	count = 0;
 	while (str[i] != 0)
 	{
-		if (!(char_in_set(str[i], charset)))
+		if (str[i] != c)
 		{
-			if (i == 0 || char_in_set(str[i - 1], charset))
+			if (i == 0 || str[i - 1] == c)
 			{
 				count++;
 				length = 0;
@@ -85,7 +73,7 @@ static int	word_length(char *str, char *charset, int index)
 	return (length);
 }
 
-static int	word_index(char *str, char *charset, int index)
+static int	word_index(char const *str, char c, int index)
 {
 	int	i;
 	int	count;
@@ -94,9 +82,9 @@ static int	word_index(char *str, char *charset, int index)
 	count = 0;
 	while (str[i] != 0)
 	{
-		if (!(char_in_set(str[i], charset)))
+		if (str[i] != c)
 		{
-			if (i == 0 || char_in_set(str[i - 1], charset))
+			if (i == 0 || str[i - 1] == c)
 			{
 				if (count == index)
 					return (i);
@@ -108,7 +96,7 @@ static int	word_index(char *str, char *charset, int index)
 	return (-1);
 }
 
-static int	count_words(char *str, char *charset)
+static int	count_words(char const *str, char c)
 {
 	int	i;
 	int	count;
@@ -117,9 +105,9 @@ static int	count_words(char *str, char *charset)
 	count = 0;
 	while (str[i] != 0)
 	{
-		if (!(char_in_set(str[i], charset)))
+		if (str[i] != c)
 		{
-			if (i == 0 || char_in_set(str[i - 1], charset))
+			if (i == 0 || str[i - 1] == c)
 				count++;
 		}
 		i++;
